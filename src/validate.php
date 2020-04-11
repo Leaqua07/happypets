32975c02f422900d1c2f22ff77cc6b41a284d299
Leaqua07 <lea.pellikan@gmail.com>
2020-04-11 13:59:23 +0200

    changed something..
    -added some other sides
    -php code gets printed....

---
+++ src/validate.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+</head>
+<body>
+
+<?php
+	$testemail = "tim@lala.de";
+	$testpasswort = "1234";
+
+	if(isset($_POST['login'])){
+		$email = $_POST['email'];
+		$pass = $_POST['passwort'];
+		if($email == $testemail and $pass == $testpasswort){
+			if(isset($_POST['remember'])){
+				setcookie('email', $email ,time()+60*60*7);		
+			}
+			session_start();
+			$_SESSION['email'] = $email;
+			header("location: welcome.php");
+		} else{
+			echo "Email or Passwort incorrect";
+		} 
+	} else {
+		header("location:login.php"); //verhindert, dass es möglich ist per link den Login zu übersprinen
+	}
+
+	?>
+
+	</body>
+	</html>
