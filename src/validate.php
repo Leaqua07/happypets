a330bfbe443a17703c05f71e39fa1a8ec9bbae96
Leaqua07 <lea.pellikan@gmail.com>
2020-04-20 19:39:58 +0200

    datenbank login
    -Datenbankabfrage für email und passwort in validate.php eingefügt -> funkt
    -tims neues Loginlayout funkt noch nocht

--- src/validate.php
+++ src/validate.php
@@ -6,26 +6,56 @@
 <body>
 
 <?php
+error_reporting(0); //nötig für schöne Fehlerausgabe
+$db = new mysqli('localhost', 'root', 'root', 'happypets');
 
-	$testemail = "tim@lala.de";
-	$testpasswort = "1234";
+$db->set_charset('utf8'); //umlaute werden umgewandelt
+print_r ($db->connect_error); //nötig für schöne Fehlerausgabe
+
+if ($db->connect_errno) {
+    die('Sorry - gerade gibt es ein Problem');
+}
+//-----------------------------------------
+
+//
+$erg = $db->query("SELECT email, passwort FROM user") or die($db->error); // or die($db->error); gibt SQL Fehler aus
+
+//print_r($erg); //Dateninformationen
+
+if ($erg->num_rows) {
+	//echo "<p>Daten vorhanden: Anzahl ";
+	//echo $erg->num_rows; // Anzahl der Datensätze
+}
+
+
+
+	//$testemail = "tim@lala.de";
+	//$testpasswort = "1234";
 
 	if(isset($_POST['login'])){
 		$email = $_POST['email'];
 		$pass = $_POST['passwort'];
-		if($email == $testemail and $pass == $testpasswort){
-			if(isset($_POST['remember'])){
-				setcookie('email', $email ,time()+60*60*7);		
-			}
-			session_start();
-			$_SESSION['email'] = $email;
-			header("location: ../index.html"); //Eingeloggte Weiterleitungsseite, evtl noch ändern
-		} else{
-			echo "Email or Passwort incorrect";
-		} 
+		//-DB----------------------
+		while ($zeile = $erg->fetch_object()) {
+		$testemail =$zeile->email;
+		$testpasswort = $zeile->passwort;
+	
+			if($email == $testemail and $pass == $testpasswort){
+				if(isset($_POST['remember'])){
+					setcookie('email', $email ,time()+60*60*7);		
+				}
+				session_start();
+				$_SESSION['email'] = $email;
+				header("location: ../index.html"); //Eingeloggte Weiterleitungsseite, evtl noch ändern
+			} else{
+				echo "Email or Passwort incorrect";
+			} 
+		}
 	} else {
 		header("location:login.php"); //verhindert, dass es möglich ist per link den Login zu übersprinen
 	}
+		$erg->free();
+		$db->close();
 
 	?>
 
