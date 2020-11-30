e32566759f5303004666a3b5241c6c248025c8fc
Leaqua07 <lea.pellikan@gmail.com>
2020-11-30 10:57:38 +0100

    iwas angepasst

--- src/logout.php
+++ src/logout.php
@@ -1,19 +1,21 @@
 <?php
-	session_start();
-    session_destroy();
-    #header('location:../index.php');
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
-if (isset($_POST['logout'])) {
-    
-	if (isset($_COOKIE['email']) and isset($_COOKIE['pass' ])){
-		$email = $_COOKIE['email'];
-		$pass = $_COOKIE['pass'];
-		setcookie('email', $email, time()-1);
-		setcookie('pass', $pass, time()-1);
-	}
-	echo "You successfully logged out";
-    
+session_start();
+session_destroy();
+#header('location:../index.php');
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+if (isset($_POST['logout']))
+{
+
+    if (isset($_COOKIE['email']) and isset($_COOKIE['pass']))
+    {
+        $email = $_COOKIE['email'];
+        $pass = $_COOKIE['pass'];
+        setcookie('email', $email, time() - 1);
+        setcookie('pass', $pass, time() - 1);
+    }
+    echo "You successfully logged out";
+
 }
-    print_r($_SESSION);
+print_r($_SESSION);
 
 ?>
--- src/postcomment.php
+++ src/postcomment.php
@@ -1,36 +1,33 @@
-        
 <?php
 session_start();
 
 //Datenbankverbingdung wird aufgebaut
-  error_reporting(0); //nötig für schöne Fehlerausgabe
-        $db1 = new mysqli('localhost', 'root', 'root', 'happypets');
+error_reporting(0); //nötig für schöne Fehlerausgabe
+$db1 = new mysqli('localhost', 'root', 'root', 'happypets');
 
-        $db1->set_charset('utf8'); //umlaute werden umgewandelt
-        print_r ($db->connect_error); //nötig für schöne Fehlerausgabe
+$db1->set_charset('utf8'); //umlaute werden umgewandelt
+print_r($db->connect_error); //nötig für schöne Fehlerausgabe
+if ($db1->connect_errno)
+{
+    die('Sorry - gerade gibt es ein Problem');
+}
 
-        if ($db1->connect_errno) {
-            die('Sorry - gerade gibt es ein Problem');
-        }
-
-if (isset($_POST['send'])){ //Wenn sende-Button gedrückt wurde
-   
+if (isset($_POST['send']))
+{ //Wenn sende-Button gedrückt wurde
     //datenbankverbindungsaufbau
     echo hallo;
     $myuser_id = $_SESSION['user_id'];
-    $mybenutzername =  $_SESSION['user_id'];
-    $myemail =  $_SESSION['email'];
+    $mybenutzername = $_SESSION['user_id'];
+    $myemail = $_SESSION['email'];
     $myeingeloggt = $_SESSION['eingeloggt'];
-    $comment =  mysqli_real_escape_string($db1,$_POST['comment']);
-    $tier_id =  $_POST['tierid'];
-    
-    $query ="INSERT INTO comments (user_id, comment, timestamp, tier_id) VALUES ('$myuser_id', '$comment',NOW(), '$tier_id')";
-    $eintragen = mysqli_query($db1, $query );
+    $comment = mysqli_real_escape_string($db1, $_POST['comment']);
+    $tier_id = $_POST['tierid'];
+
+    $query = "INSERT INTO comments (user_id, comment, timestamp, tier_id) VALUES ('$myuser_id', '$comment',NOW(), '$tier_id')";
+    $eintragen = mysqli_query($db1, $query);
     header('location: animal.php?tierid=' . $_POST['tierid']);
-    }
-    $erg->free();
-    $db1->close();
+}
+$erg->free();
+$db1->close();
 
-
-
-    ?>
+?>
